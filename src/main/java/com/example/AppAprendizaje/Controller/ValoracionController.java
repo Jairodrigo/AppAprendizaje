09752cb9c43cc46ac1b68fa3c0ce5e71cc4db9d1
Jairodrigo <jairodrigo2003@gmail.com>
2025-12-09 13:49:45 -0500

    Update CORS origin for ValoracionController

--- src/main/java/com/example/AppAprendizaje/Controller/ValoracionController.java
+++ src/main/java/com/example/AppAprendizaje/Controller/ValoracionController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/valoraciones")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://app-aprendizaje-front.vercel.app")
 public class ValoracionController {
 
     @Autowired
