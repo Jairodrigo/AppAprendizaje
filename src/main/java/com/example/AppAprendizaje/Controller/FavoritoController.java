04000206df152dfaaa815a3d0a21d978d33fedd1
Jairodrigo <jairodrigo2003@gmail.com>
2025-12-09 13:48:40 -0500

    Update CORS origin for FavoritoController

--- src/main/java/com/example/AppAprendizaje/Controller/FavoritoController.java
+++ src/main/java/com/example/AppAprendizaje/Controller/FavoritoController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/favoritos")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://app-aprendizaje-front.vercel.app")
 public class FavoritoController {
 
     private final FavoritoService favoritoService;
