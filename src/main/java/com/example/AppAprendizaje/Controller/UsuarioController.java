53d8366f48f6432f05f86d2acc169a663ec9ae1e
Jairodrigo <jairodrigo2003@gmail.com>
2025-12-09 13:49:34 -0500

    Update CORS origin for UsuarioController

--- src/main/java/com/example/AppAprendizaje/Controller/UsuarioController.java
+++ src/main/java/com/example/AppAprendizaje/Controller/UsuarioController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/usuarios")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://app-aprendizaje-front.vercel.app")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
